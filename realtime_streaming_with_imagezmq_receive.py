22565d7549b9fd73383eb53f5ead9272cf7d304c
gangbacol <gangyeonjo336@gmail.com>
2020-03-04 04:06:20 +0900

    Update realtime_streaming_with_imagezmq_receive.py.

--- realtime_streaming_with_imagezmq_receive.py
+++ realtime_streaming_with_imagezmq_receive.py
@@ -5,6 +5,9 @@ import imagezmq
 import argparse
 import imutils
 import cv2
+import time
+import socket
+import threading
 
 def getOutputsNames(net):
     layersNames = net.getLayerNames()
@@ -23,29 +26,26 @@ def postprocess(frame, outs):
             classId = np.argmax(scores)
             if classes[classId] != 'stop sign':
                 continue
+            print(str(classes[classId])+' detect!')
             confidence = scores[classId]
             if confidence > confThreshold:
-                center_x = int(detection[0] * frameWidth)
-                center_y = int(detection[1] * frameHeight)
-                width = int(detection[2] * frameWidth)
-                height = int(detection[3] * frameHeight)
-                left = int(center_x - width / 2)
-                top = int(center_y - height / 2)
+                box = detection[0:4] * np.array([frameWidth, frameHeight, frameWidth, frameHeight])
+                (center_x, center_y, width, height) = box.astype("int")
+                x = int(center_x - width / 2)
+                y = int(center_y - height / 2)
                 classIds.append(classId)
                 confidences.append(float(confidence))
-                boxes.append([left, top, width, height])
+                boxes.append([x, y, int(width), int(height)])
     indices = cv2.dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThreshold)
     for i in indices:
         i = i[0]
         box = boxes[i]
-        left = box[0]
-        top = box[1]
-        width = box[2]
-        height = box[3]
-        drawPred(classIds[i], confidences[i], left, top, left + width, top + height)
+        (x, y) = (box[0], box[1])
+        (width, height) = (box[2], box[3])
+        drawPred(classIds[i], confidences[i], x, y, x + width, y + height)
 
 def drawPred(classId, conf, left, top, right, bottom):
-    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255))
+    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
     
     label = '%.2f' % conf
     label = '%s:%s' % (classes[classId], label)
@@ -54,14 +54,26 @@ def drawPred(classId, conf, left, top, right, bottom):
     top = max(top, labelSize[1])
     cv2.putText(frame, label, (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
 
+def receiveDistance():
+    while True:
+        print(client_socket.recv(1024))
+
 # construct the argument parser and parse the arguments
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(('192.168.219.110', 9999))
+server_socket.listen()
+client_socket, addr = server_socket.accept()
+print('Connected by', addr)
+
+
 imageHub = imagezmq.ImageHub()
 
 confThreshold = 0.5
 nmsThreshold = 0.4
 
-modelCfg = '/Users/gangbacol/Desktop/Development/socket_RaspberryPi_Mac/model&weight/yolov3.cfg'
-modelWeight = '/Users/gangbacol/Desktop/Development/socket_RaspberryPi_Mac/model&weight/yolov3.weights'
+modelCfg = '/Users/gangbacol/Desktop/Development/socket_RaspberryPi_Mac/model&weight/yolov3-tiny.cfg'
+modelWeight = '/Users/gangbacol/Desktop/Development/socket_RaspberryPi_Mac/model&weight/yolov3-tiny.weights'
 classesFile = '/Users/gangbacol/Desktop/Development/socket_RaspberryPi_Mac/model&weight/coco.names'
 
 with open(classesFile, 'rt') as f:
@@ -71,22 +83,27 @@ with open(classesFile, 'rt') as f:
 print("[INFO] loading model...")
 net = cv2.dnn.readNetFromDarknet(modelCfg, modelWeight)
 
+count = 0
+t = threading.Thread(target=receiveDistance)
+t.start()
 while True:
     (rpiName, frame) = imageHub.recv_image()
     imageHub.send_reply(b'OK')
     print("[INFO] Predicting will start...")
     
-    frame = imutils.resize(frame, width=400)
+    frame = imutils.resize(frame, width=800)
     (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (320, 320)), 1/255, (320, 320), [0,0,0], 1, crop=False)
+    
+    cv2.imshow('input frame(GrayScale)', cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
 
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (320, 320)), 1/255.0, (320, 320), [0,0,0], 1, crop=False)
+    
     net.setInput(blob)
     outs = net.forward(getOutputsNames(net))
     outs = np.array(outs)
 
-    print(outs.shape)
     postprocess(frame, outs)
-
+    
     t, _ = net.getPerfProfile()
     label = 'Inference time: %.2f ms' % (t * 1000.0 / cv2.getTickFrequency())
     cv2.putText(frame, label, (0, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
@@ -95,5 +112,5 @@ while True:
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
         break
-
+    count += 1
 cv2.destroyAllWindows()
