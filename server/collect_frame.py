de8fdc677135c4cae6839185cda40261aa5330a6
gangbacol <gangyeonjo336@gmail.com>
2020-03-06 03:56:38 +0900

    Update collect_frame.py.

--- server/collect_frame.py
+++ server/collect_frame.py
@@ -1,15 +1,17 @@
 import imagezmq
 import cv2
 import imutils
+import socket
 from time import sleep
 
 save_path = '/Users/gangbacol/Desktop/Development/socket_RaspberryPi_Mac/training_dataset'
 
-imageHub = imagezmq.ImageHub
+imageHub = imagezmq.ImageHub()
 
 count = 0
-
+print("start")
 while True:
+    print("count: "+str(count+1))
     (rpiName, frame) = imageHub.recv_image()
     imageHub.send_reply(b'ok')
     frame = imutils.resize(frame, width=500)
@@ -17,6 +19,7 @@ while True:
     if count < 10 : cv2.imwrite(save_path+'/frame_000'+str(count+1)+'.jpg', frame)
     elif count < 100 : cv2.imwrite(save_path+'/frame_00'+str(count+1)+'.jpg', frame)
     elif count < 1000 : cv2.imwrite(save_path+'/frame_0'+str(count+1)+'.jpg', frame)
-    else cv2.imwrite(save_path+'/frame_'+str(count+1)+'.jpg', frame)
-
+    else : cv2.imwrite(save_path+'/frame_'+str(count+1)+'.jpg', frame)
+    
+    count += 1
     sleep(0.05)
