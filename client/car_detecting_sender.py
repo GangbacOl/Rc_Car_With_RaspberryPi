3d8090521474303dbc87d7050b46098b43562976
GangbacOl <gangyeonjo336@gmail.com>
2020-03-31 16:13:34 +0900

    Rename realtime_streaming_with_imagezmq.py -> car_detecting_sender.py.

--- client/car_detecting_sender.py
+++ client/car_detecting_sender.py
@@ -8,6 +8,7 @@ import threading
 
 def ultrasound() :
 	while True:
+		global distance
 		gpio.output(trig, False)
 		time.sleep(0.5)
 		gpio.output(trig, True)
@@ -19,15 +20,43 @@ def ultrasound() :
 			pulse_end = time.time()
 		pulse_duration = pulse_end - pulse_start
 		distance = pulse_duration * 17000
-		distance = round(distance, 2)	
-		client_socket.send(str(distance).encode())
+		distance = round(distance, 2)
 
-gpio.setmode(gpio.BCM)
+def receiveStopSign():
+	while True:
+		client_socket.send(str(distance).encode())
+		sign = client_socket.recv(1024)
+		if sign:
+			global drive_flag
+			print('stop')
+			pwm_forward_1.ChangeDutyCycle(0)
+			pwm_forward_2.ChangeDutyCycle(0)
+			gpio.output(in_1_forward, 1)
+			gpio.output(in_2_forward, 1) 
+			drive_flag = False
+			break
+
+distance = 0
+drive_flag = True
+
 trig = 13
 echo = 19
+in_1_forward = 18
+in_2_forward = 23
 
+gpio.setmode(gpio.BCM)
 gpio.setup(trig, gpio.OUT)
 gpio.setup(echo, gpio.IN)
+gpio.setup(in_1_forward, gpio.OUT)
+gpio.setup(in_2_forward, gpio.OUT)
+
+pwm_forward_1 = gpio.PWM(in_1_forward, 100)
+pwm_forward_2 = gpio.PWM(in_2_forward, 100)
+
+pwm_forward_1.start(0)
+pwm_forward_2.start(0)
+
+pwm_forward_1.ChangeDutyCycle(30)
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-s", "--server-ip", required=True,
@@ -43,10 +72,12 @@ rpiName = socket.gethostname()
 vs = VideoStream(usePiCamera=True).start()
 
 time.sleep(2.0)
-t = threading.Thread(target=ultrasound)
-t.start()
+t1 = threading.Thread(target=receiveStopSign)
+t1.start()
+t2 = threading.Thread(target=ultrasound)
+t2.start()
 
-while True:
+while drive_flag:
 	frame = vs.read()
 	sender.send_image(rpiName, frame)
-vs.close()
+	gpio.output(in_2_forward, 0)
